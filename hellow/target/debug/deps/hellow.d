15077a63a5851cc1e8fd11b1cc955b08e3c3d5ba
owiger-8 <pssivadeth@gmail.com>
2025-05-04 14:45:17 +0530

    1

---
+++ hellow/target/debug/deps/hellow.d
@@ -0,0 +1,5 @@
+C:\Users\psx\Documents\projects\rust-beginner\hellow\target\debug\deps\hellow.d: src\main.rs
+
+C:\Users\psx\Documents\projects\rust-beginner\hellow\target\debug\deps\hellow.exe: src\main.rs
+
+src\main.rs:
---
+++ hellow/target/debug/hellow.d
@@ -0,0 +1,1 @@
+C:\Users\psx\Documents\projects\rust-beginner\hellow\target\debug\hellow.exe: C:\Users\psx\Documents\projects\rust-beginner\hellow\src\main.rs
