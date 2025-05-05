732b0da9cafa4dd54536ac8dd8d9d4876812c64c
owiger-8 <pssivadeth@gmail.com>
2025-05-05 14:52:29 +0530

    2

---
+++ temp_converter/target/debug/deps/temp_converter.d
@@ -0,0 +1,5 @@
+C:\Users\psx\Documents\projects\rust-beginner\temp_converter\target\debug\deps\temp_converter.d: src\main.rs
+
+C:\Users\psx\Documents\projects\rust-beginner\temp_converter\target\debug\deps\temp_converter.exe: src\main.rs
+
+src\main.rs:
---
+++ temp_converter/target/debug/temp_converter.d
@@ -0,0 +1,1 @@
+C:\Users\psx\Documents\projects\rust-beginner\temp_converter\target\debug\temp_converter.exe: C:\Users\psx\Documents\projects\rust-beginner\temp_converter\src\main.rs
