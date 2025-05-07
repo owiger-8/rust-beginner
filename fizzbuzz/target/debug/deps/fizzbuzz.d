8f6abecf95e23084e026e00615e53786c6f10342
owiger-8 <pssivadeth@gmail.com>
2025-05-07 21:19:54 +0530

    4

---
+++ fizzbuzz/target/debug/deps/fizzbuzz.d
@@ -0,0 +1,5 @@
+C:\Users\psx\Documents\projects\rust-beginner\fizzbuzz\target\debug\deps\fizzbuzz.d: src\main.rs
+
+C:\Users\psx\Documents\projects\rust-beginner\fizzbuzz\target\debug\deps\fizzbuzz.exe: src\main.rs
+
+src\main.rs:
---
+++ fizzbuzz/target/debug/fizzbuzz.d
@@ -0,0 +1,1 @@
+C:\Users\psx\Documents\projects\rust-beginner\fizzbuzz\target\debug\fizzbuzz.exe: C:\Users\psx\Documents\projects\rust-beginner\fizzbuzz\src\main.rs
