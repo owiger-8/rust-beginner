827948ebc297849244d9b6bc9cb9ed0e38f1822e
owiger-8 <pssivadeth@gmail.com>
2025-05-09 21:46:05 +0530

    6

---
+++ factorial/target/debug/deps/factorial.d
@@ -0,0 +1,5 @@
+C:\Users\psx\Documents\projects\rust-beginner\factorial\target\debug\deps\factorial.d: src\main.rs
+
+C:\Users\psx\Documents\projects\rust-beginner\factorial\target\debug\deps\factorial.exe: src\main.rs
+
+src\main.rs:
---
+++ factorial/target/debug/factorial.d
@@ -0,0 +1,1 @@
+C:\Users\psx\Documents\projects\rust-beginner\factorial\target\debug\factorial.exe: C:\Users\psx\Documents\projects\rust-beginner\factorial\src\main.rs
