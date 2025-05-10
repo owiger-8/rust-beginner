342f29d192c3a35b4aac4ae62a4efcf795b5ad89
owiger-8 <pssivadeth@gmail.com>
2025-05-10 21:54:19 +0530

    7

---
+++ fibonacci/target/debug/deps/fibonacci.d
@@ -0,0 +1,5 @@
+C:\Users\psx\Documents\projects\rust-beginner\fibonacci\target\debug\deps\fibonacci.d: src\main.rs
+
+C:\Users\psx\Documents\projects\rust-beginner\fibonacci\target\debug\deps\fibonacci.exe: src\main.rs
+
+src\main.rs:
---
+++ fibonacci/target/debug/fibonacci.d
@@ -0,0 +1,1 @@
+C:\Users\psx\Documents\projects\rust-beginner\fibonacci\target\debug\fibonacci.exe: C:\Users\psx\Documents\projects\rust-beginner\fibonacci\src\main.rs
