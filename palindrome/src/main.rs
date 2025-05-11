b4fced52d9ccd66e9168812a0ad268a075a3df44
owiger-8 <pssivadeth@gmail.com>
2025-05-11 22:05:33 +0530

    8

--- palindrome/src/main.rs
+++ palindrome/src/main.rs
@@ -1,3 +1,25 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+
+    let mut s = String::new();
+    io::stdin()
+        .readling(&mut s);
+        .except("Couldn't read ");
+
+    if ispal(&input) {
+        println("palindrome");
+    }else {
+        println("Not palindrome");
+    }
+}
+fn ispal( t : &str) -> bool {
+    let clean : String = t
+    .chars()
+    .filter(|c| c.is_alphanumeric())
+    .map(|c| c.to_ascii_lowercase())
+    .collect();
+
+    let reversed : String = cleaned.chars().rev().collect();
+
+    cleaned == reversed
 }
