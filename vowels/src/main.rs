59b5d96668da085611fe90261413d521a8bf3503
owiger-8 <pssivadeth@gmail.com>
2025-05-12 22:18:29 +0530

    9

--- vowels/src/main.rs
+++ vowels/src/main.rs
@@ -1,3 +1,26 @@
+use std::io ; 
 fn main() {
-    println!("Hello, world!");
+    
+    
+    let mut s = String::new()  ;
+    io::stdin()
+        .read_line(&mut s)
+        .expect("no input");
+
+    let count = vowels(&s);
+    println!( "Number of vowels {}" . count );
+} 
+fn count ( sen : String) -> usize {
+    let mut count = 0 ;
+
+    for c in sen.chars(){
+        match c.to_ascii_lowercase(){
+            'a' | 'e' | 'i' | 'o' | 'u'  => {
+                count += 1;
+            }
+
+        }
+
+    }
+    count
 }
