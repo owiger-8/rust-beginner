07633917f1fac9d631b08ca9e9d53e33b72cd48d
owiger-8 <pssivadeth@gmail.com>
2025-05-13 22:29:03 +0530

    10

---
+++ words/target/debug/deps/words.d
@@ -0,0 +1,5 @@
+C:\Users\psx\Documents\projects\rust-beginner\words\target\debug\deps\words.d: src\main.rs
+
+C:\Users\psx\Documents\projects\rust-beginner\words\target\debug\deps\words.exe: src\main.rs
+
+src\main.rs:
---
+++ words/target/debug/words.d
@@ -0,0 +1,1 @@
+C:\Users\psx\Documents\projects\rust-beginner\words\target\debug\words.exe: C:\Users\psx\Documents\projects\rust-beginner\words\src\main.rs
